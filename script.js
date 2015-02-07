214d3871f6d22ecf56d0e341bed7149eed0318f9
cadkinson <cwadkinson@gmail.com>
2015-02-07 14:31:10 -0800

    Added script.js and modified site to include jQuery interaction with the ingredients checklist

---
+++ script.js
@@ -0,0 +1,1 @@
+<script language="javascript">$(function(){$("#selectall").click(function(){$('.case').attr('checked',this.checked);});$(".case").click(function(){if($(".case").length==$(".case:checked").length){$("#selectall").attr("checked","checked");}else{$("#selectall").removeAttr("checked");}});});</script>
